231fd38a419ef4c0424ab0e670a8c65aff9b77f9
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-18 16:56:10 +0200

    mhh

---
+++ src/doomgeneric_profan.c
@@ -0,0 +1,60 @@
+//doomgeneric for cross-platform development library 'Simple DirectMedia Layer'
+
+#include "doomkeys.h"
+#include "m_argv.h"
+#include "doomgeneric.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <stdbool.h>
+
+#define KEYQUEUE_SIZE 16
+
+
+static unsigned char convertToDoomKey(unsigned int key) {
+  return 0;
+}
+
+static void addKeyToQueue(int pressed, unsigned int keyCode) {
+  return;
+}
+
+static void handleKeyInput() {
+  return;
+}
+
+
+void DG_Init() {
+  return;
+}
+
+void DG_DrawFrame() {
+  return;
+}
+
+void DG_SleepMs(uint32_t ms) {
+  return;
+}
+
+uint32_t DG_GetTicksMs() {
+  return c_timer_get_ms();
+}
+
+int DG_GetKey(int* pressed, unsigned char* doomKey) {
+  return 0;
+}
+
+void DG_SetWindowTitle(const char * title) {
+  return;
+}
+
+int main(int argc, char **argv) {
+    doomgeneric_Create(argc, argv);
+
+    for (int i = 0; ; i++) {
+        doomgeneric_Tick();
+    }
+
+    return 0;
+}
