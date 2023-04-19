b5f7e9408056d27635c42bc72524a45badad675c
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-19 12:58:15 +0200

    Update doomgeneric_profan.c

--- src/doomgeneric_profan.c
+++ src/doomgeneric_profan.c
@@ -6,18 +6,68 @@
 
 #include <syscall.h>
 
-#define KEYQUEUE_SIZE 16
-
+uint32_t *current_screen;
 
 void DG_Init() {
-    return;
+    current_screen = calloc(DOOMGENERIC_RESX * DOOMGENERIC_RESY, sizeof(uint32_t));
 }
 
+uint8_t convertToDoomKey(uint8_t scancode) {
+    uint8_t key = 0;
+
+    switch (scancode) {
+    case 0x9C:
+    case 0x1C:
+        key = KEY_ENTER;
+        break;
+    case 0x01:
+        key = KEY_ESCAPE;
+        break;
+    case 0xCB:
+    case 0x4B:
+        key = KEY_LEFTARROW;
+        break;
+    case 0xCD:
+    case 0x4D:
+        key = KEY_RIGHTARROW;
+        break;
+    case 0xC8:
+    case 0x48:
+        key = KEY_UPARROW;
+        break;
+    case 0xD0:
+    case 0x50:
+        key = KEY_DOWNARROW;
+        break;
+    case 0x1D:
+        key = KEY_FIRE;
+        break;
+    case 0x39:
+        key = KEY_USE;
+        break;
+    case 0x2A:
+    case 0x36:
+        key = KEY_RSHIFT;
+        break;
+    case 0x15:
+        key = 'y';
+        break;
+    default:
+        break;
+    }
+
+    return key;
+}
+
+
 void DG_DrawFrame() {
-    serial_debug("drawing frame\n");
-    for (int x = 0; x < DOOMGENERIC_RESX; x++) {
-        for (int y = 0; y < DOOMGENERIC_RESY; y++) {
-            c_vesa_set_pixel(x, y, DG_ScreenBuffer[x + y * DOOMGENERIC_RESX]);
+    int pos = 0;
+    for (int y = 0; y < DOOMGENERIC_RESY; y++) {
+        for (int x = 0; x < DOOMGENERIC_RESX; x++) {
+            pos++;
+            if (current_screen[pos] == DG_ScreenBuffer[pos]) continue;
+            current_screen[pos] = DG_ScreenBuffer[pos];
+            c_vesa_set_pixel(x, y, DG_ScreenBuffer[pos]);
         }
     }
 }
@@ -28,13 +78,32 @@ void DG_SleepMs(uint32_t ms) {
 }
 
 uint32_t DG_GetTicksMs() {
-    serial_debug("getting ticks\n");
     return c_timer_get_ms();
 }
 
-int DG_GetKey(int* pressed, unsigned char* doomKey) {
-    serial_debug("getting key\n");
-    return 0;
+int DG_GetKey(int* pressed, uint8_t* doomKey) {
+    uint8_t scancode, key;
+
+    scancode = (uint8_t) c_kb_get_scfh();
+    if (scancode == 0) return 0;
+
+    serial_debug("scancode: %d\n", scancode);
+
+    if (scancode > 127) {
+        scancode -= 128;
+        *pressed = 0;
+    } else {
+        *pressed = 1;
+    }
+
+    key = convertToDoomKey(scancode);
+    if (key == 0) return 0;
+
+    *doomKey = key;
+
+    serial_debug("key: %d, pressed: %d\n", *doomKey, *pressed);
+
+    return 1;
 }
 
 void DG_SetWindowTitle(const char * title) {
@@ -46,7 +115,6 @@ int main(int argc, char **argv) {
     doomgeneric_Create(argc, argv);
 
     for (int i = 0; ; i++) {
-        serial_debug("tick %d\n", i);
         doomgeneric_Tick();
     }
 
