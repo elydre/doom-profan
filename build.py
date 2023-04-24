c9ea0789cd9c335457cab83175e649ae1b9234c9
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-24 12:07:28 +0200

    scuba

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import os
 CC      = "gcc"
 LD      = "ld"
 
-OUTPUT  = "doom-core"
+OUTPUT  = "doom"
 
 CFLAGS  = "-ffreestanding -fno-exceptions -fno-stack-protector -m32 -I ./profan_zlib -I ./local_lib"
 LDFLAGS = "-T link.ld"
@@ -38,7 +38,6 @@ def main():
     entry = compile_file("entry.c", ".")
     link_files(entry, objs)
 
-    link_files(compile_file("doom.c", "."), "", "doom")
     execute_command("rm *.pe")
 
 if __name__ == "__main__":
