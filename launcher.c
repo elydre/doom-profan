994a730220415973bd329ccf1c41e67c29692653
pf4 <74263128+elydre@users.noreply.github.com>
2023-04-29 11:55:09 +0200

    no launcher

--- launcher.c
+++
@@ -1,6 +0,0 @@
-#include <syscall.h>
-
-int entry(int argc, char **argv) {
-    // we need this launcher to give more virtual 
-    c_run_ifexist_full("/bin/other/doom-core.bin", argc, argv, 0xC0000000, 0x100000, 0x10000);
-}
