71377d8bc5b3d0ff3a2e5a9f1b5299e80afde0c1
pf4 <74263128+elydre@users.noreply.github.com>
2024-03-30 13:02:56 +0100

    yes

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ LD      = "ld"
 OUTPUT  = "doom"
 
 CFLAGS  = "-ffreestanding -fno-exceptions -fno-stack-protector -m32 -I ./profan_zlib -I ./local_lib"
-LDFLAGS = "-T link.ld"
+LDFLAGS = "-nostdlib -L../profanOS/out/zlibs -T link.ld -z max-page-size=0x1000 -lc -lm"
 
 OBJDIR  = "build"
 SRCDIR  = "src"
@@ -27,8 +27,7 @@ def compile_file(src, dir = SRCDIR):
     return obj
 
 def link_files(entry, objs, output = OUTPUT):
-    execute_command(f"{LD} {LDFLAGS} -o {output}.pe {entry} {' '.join(objs)}")
-    execute_command(f"objcopy -O binary {output}.pe {output}.bin")
+    execute_command(f"{LD} {LDFLAGS} -o {output}.elf {entry} {' '.join(objs)}")
 
 def main():
     execute_command(f"mkdir -p {OBJDIR}")
@@ -37,8 +36,6 @@ def main():
 
     entry = compile_file("entry.c", ".")
     link_files(entry, objs)
-
-    execute_command("rm *.pe")
 
 if __name__ == "__main__":
     main()
